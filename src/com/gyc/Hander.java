7d0a628de30c8488e4afafc0d2fa8c1b38541441
guoyecheng <guoyecheng0207@163.com>
2014-06-18 11:24:48 +0800

    搭建spring框架

--- src/com/gyc/Hander.java
+++ src/com/gyc/Hander.java
@@ -1,5 +1,20 @@
 package com.gyc;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/api")
 public class Hander {
 
+	@RequestMapping(params="hander", method = RequestMethod.GET)
+	public void hander(HttpServletRequest request, HttpServletResponse response) throws Exception{
+		
+		String echostr = request.getParameter("echostr");
+		response.getWriter().print(echostr);
+	}
 }
